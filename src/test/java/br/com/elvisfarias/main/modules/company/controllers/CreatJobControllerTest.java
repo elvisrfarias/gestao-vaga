5a04d0789f2eb7bc506d8772898a6d33aa5ef6c4
Elvis Farias <elvisrfarias1@gmail.com>
2025-06-01 09:54:36 -0300

    Add tratamento e formatação no teste

--- src/test/java/br/com/elvisfarias/main/modules/company/controllers/CreatJobControllerTest.java
+++ src/test/java/br/com/elvisfarias/main/modules/company/controllers/CreatJobControllerTest.java
@@ -4,8 +4,11 @@ import br.com.elvisfarias.main.modules.company.dto.CreateJobDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -13,6 +16,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CreatJobControllerTest {
 
     private MockMvc mvc;
@@ -26,7 +31,7 @@ public class CreatJobControllerTest {
     }
 
     @Test
-    public void sholdBeAbleToCreateNewJob() {
+    public void sholdBeAbleToCreateNewJob() throws Exception {
         var createJobDTO = CreateJobDTO.builder()
                 .benefits("Benefits_teste")
                 .level("Level_teste")
@@ -34,10 +39,12 @@ public class CreatJobControllerTest {
                 .build();
 
 
-        mvc.perform(MockMvcRequestBuilders.post("/company/job")
+       var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(createJobDTO))
-        ).andExpect(status().isOk());
+                .content(asJsonString(createJobDTO)))
+                .andExpect(status().isOk());
+
+        System.out.println(result);
     }
 
     private static String asJsonString(Object obj) {
